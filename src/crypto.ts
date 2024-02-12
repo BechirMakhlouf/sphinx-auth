abaf4b28f3d08f5b0811d31541797987abaed3b2
bechirMakhlouf <bechirM@proton.me>
2024-02-12 12:34:05 +0100

    Move IHasher to the crypto module

--- src/crypto.ts
+++ src/crypto.ts
@@ -1,1 +1,46 @@
 // import { scrypt } from "hash-wasm";
+
+import { bcrypt, bcryptVerify } from "hash-wasm";
+import { type IDataType } from "hash-wasm/dist/lib/util";
+import * as crypto from "node:crypto";
+
+export interface IHasher {
+  /**
+   * @param hashes str
+   */
+  hash: (str: IDataType) => Promise<string>;
+
+  /**
+   * verifies if the password matches a given hash
+   * @param string - the password to verify
+   * @param hashedString - the hashed password
+   *
+   * @returns boolean if the password is correct
+   */
+  verify: (password: IDataType, hashedPassword: string) => Promise<boolean>;
+}
+
+export class BcryptHasher implements IHasher {
+  readonly DEFAULT_COST_FACTOR = 10
+  private readonly costFactor: number = this.DEFAULT_COST_FACTOR;
+
+  async hash(str: IDataType): Promise<string> {
+    return await bcrypt({
+      password: str,
+      salt: crypto.randomBytes(16),
+      costFactor: this.costFactor,
+    });
+  }
+
+  async verify(password: IDataType, hashedPassword: string): Promise<boolean> {
+    return await bcryptVerify({ password, hash: hashedPassword });
+  }
+
+  /**
+   * @default costFactor default is 10
+   */
+  constructor(costFactor?: number) {
+    this.costFactor = costFactor ?? this.costFactor;
+  }
+}
+
--- src/password.ts
+++ src/password.ts
@@ -1,6 +1,5 @@
-import { bcrypt, bcryptVerify } from "hash-wasm";
-import { type IDataType } from "hash-wasm/dist/lib/util";
-import * as crypto from "node:crypto";
+import { type IHasher } from "./crypto";
+
 // Hash/Verify/
 
 export interface IPasswordHandler {
@@ -19,45 +18,6 @@ export interface IPasswordHandler {
    * @returns boolean if the password is correct
    */
   verify: (password: string, hashedPassword: string) => Promise<boolean>;
-}
-
-export interface IHasher {
-  /**
-   * @param hashes str
-   */
-  hash: (str: IDataType) => Promise<string>;
-
-  /**
-   * verifies if the password matches a given hash
-   * @param string - the password to verify
-   * @param hashedString - the hashed password
-   *
-   * @returns boolean if the password is correct
-   */
-  verify: (password: IDataType, hashedPassword: string) => Promise<boolean>;
-}
-
-export class BcryptHasher implements IHasher {
-  private readonly costFactor: number = 10;
-
-  async hash(str: IDataType): Promise<string> {
-    return await bcrypt({
-      password: str,
-      salt: crypto.randomBytes(16),
-      costFactor: this.costFactor,
-    });
-  }
-
-  async verify(password: IDataType, hashedPassword: string): Promise<boolean> {
-    return await bcryptVerify({ password, hash: hashedPassword });
-  }
-
-  /**
-   * @default costFactor default is 10
-   */
-  constructor(costFactor?: number) {
-    this.costFactor = costFactor ?? this.costFactor;
-  }
 }
 
 export class PasswordHandler implements IPasswordHandler {
