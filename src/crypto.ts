e7ef7efa08c358cb8e2a0d86ab3edf02026200fb
bechirMakhlouf <bechirM@proton.me>
2024-02-13 11:18:27 +0100

    add argon2i encryption

--- src/crypto.ts
+++ src/crypto.ts
@@ -1,6 +1,14 @@
 // import { scrypt } from "hash-wasm";
 
-import { bcrypt, bcryptVerify } from "hash-wasm";
+import {
+  type Argon2VerifyOptions,
+  argon2Verify,
+  argon2d,
+  argon2i,
+  argon2id,
+  bcrypt,
+  bcryptVerify,
+} from "hash-wasm";
 import { type IDataType } from "hash-wasm/dist/lib/util";
 import * as crypto from "node:crypto";
 
@@ -21,7 +29,7 @@ export interface IHasher {
 }
 
 export class BcryptHasher implements IHasher {
-  readonly DEFAULT_COST_FACTOR = 10
+  readonly DEFAULT_COST_FACTOR = 10;
   private readonly costFactor: number = this.DEFAULT_COST_FACTOR;
 
   async hash(str: IDataType): Promise<string> {
@@ -44,3 +52,78 @@ export class BcryptHasher implements IHasher {
   }
 }
 
+export interface Argon2Options {
+  /**
+   * Salt (usually containing random bytes)
+   */
+  salt: IDataType;
+  /**
+   * Number of iterations to perform
+   */
+  iterations: number;
+  /**
+   * Degree of parallelism
+   */
+  parallelism: number;
+  /**
+   * Amount of memory to be used in kibibytes (1024 bytes)
+   */
+  memorySize: number;
+  /**
+   * Output size in bytes
+   */
+  hashLength: number;
+  /**
+   * Desired output type. Defaults to 'hex'
+   */
+  outputType?: "hex" | "binary" | "encoded";
+}
+
+export class Argon2Hasher implements IHasher {
+  public hashingConfiguration: Argon2Options = {
+    salt: crypto.randomBytes(32), // A random salt as a string or buffer
+    iterations: 3, // Number of iterations
+    parallelism: 2, // Degree of parallelism
+    memorySize: 4096, // Memory usage in kibibytes (4MB)
+    hashLength: 32, // Output size in bytes (256 bits)
+    outputType: "encoded", // Desired output type
+  };
+
+  private readonly hasher: typeof argon2id;
+  private readonly argon2 = {
+    argon2d,
+    argon2i,
+    argon2id,
+  };
+
+  async hash(
+    str: IDataType,
+    secret?: IDataType,
+    customConfig?: Argon2Options,
+  ): Promise<string> {
+    return await this.hasher({
+      ...(customConfig ?? this.hashingConfiguration),
+      password: str,
+      secret,
+    });
+  }
+
+  async verify(
+    password: IDataType,
+    hashedPassword: string,
+    secret?: IDataType,
+  ): Promise<boolean> {
+    return await argon2Verify({ password, secret, hash: hashedPassword });
+  }
+
+  constructor(
+    argonType: "argon2i" | "argon2d" | "argon2id",
+    options?: Argon2Options,
+  ) {
+    if (options !== undefined) {
+      this.hashingConfiguration = options;
+    }
+
+    this.hasher = this.argon2[argonType];
+  }
+}
--- tests/password.test.ts
+++ tests/password.test.ts
@@ -1,10 +1,18 @@
-import { PasswordHandler, BcryptHasher } from "../src/password";
+import { PasswordHandler } from "../src/password";
+import { Argon2Hasher, BcryptHasher } from "../src/crypto";
 
-test("password hashing", async () => {
+test("bcrypt hashing", async () => {
   const passwordHandler = new PasswordHandler(new BcryptHasher());
 
   const password: string = "testing password";
   const passwordHash: string = await passwordHandler.hash(password);
 
+  expect(await passwordHandler.verify(password, passwordHash)).toBe(true);
+});
+
+test("argon2 hashing", async () => {
+  const passwordHandler = new PasswordHandler(new Argon2Hasher("argon2id"));
+  const password: string = "testing password";
+  const passwordHash: string = await passwordHandler.hash(password);
   expect(await passwordHandler.verify(password, passwordHash)).toBe(true);
 });
